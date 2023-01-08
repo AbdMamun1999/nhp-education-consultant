d9ccf49497206fbd62644692710f73070072afe8
Abdullah Al Mamun <mamun12121999@gmail.com>
2023-01-08 11:16:18 +0600

    initial project and route setup

---
+++ src/Layouts/Main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Layouts/Main/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Home;
